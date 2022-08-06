0a4ef6f4733193276632ffee6d17f273a8bed018
pavantamidala <pavantamidala023@gmail.com>
2022-08-06 08:45:24 +0530

    refactor:added note

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Milk Distribution')
-    .setDescription('Milk Distribution API description')
+    .setDescription(
+      'Milk Distribution API description *** NOTE: Date format when creating a new order ("06-08-2022") ',
+    )
     .setVersion('1.0')
     .addTag('orders')
     .build();
